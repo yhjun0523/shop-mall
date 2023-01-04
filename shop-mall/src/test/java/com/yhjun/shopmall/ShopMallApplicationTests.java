c43a8ca49d0e1b991b71094467f0676643e88317
yhjun0523 <windowish@gmail.com>
2023-01-04 13:08:03 +0900

    Project Create

---
+++ shop-mall/src/test/java/com/yhjun/shopmall/ShopMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhjun.shopmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
